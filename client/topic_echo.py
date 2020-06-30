8bc6b8f69ef8320f1c9d4152cc29ef6c98f90677
Zhengyu <zfuaj@connect.ust.hk>
2020-06-30 11:17:16 +0800

    resolve path of the config.ini

--- client/topic_echo.py
+++ client/topic_echo.py
@@ -1,15 +1,17 @@
 import roslibpy
 import sys
 import configparser
+import os
 
 config = configparser.ConfigParser()
-config.read('./config.ini')
+config_path = os.path.join(os.path.dirname(__file__), 'config.ini')
+config.read(config_path)
 
 try:
     HOST=config['server']['url']
     PORT=int(config['server']['port'])
 except KeyError:
-    print("Plesae change directory to \"client\" first and then run the script again")
+    print("Cannot find correct keys! Please fix the config file.")
     sys.exit(1)
 
 client = roslibpy.Ros(host=HOST, port=PORT)
@@ -22,7 +24,6 @@ topics = client.get_topics()
 if '/turtle1/pose' in topics:
     print('Echo /turtle1/pose')
     listener = roslibpy.Topic(client, '/turtle1/pose', 'turtlesim/Pose')
-    # listener.subscribe(lambda message: print(f"x: {message['x']} y: {message['y']}"))
     listener.subscribe(lambda message: print("x: {:.4f} y: {:.4f}".format(message['x'], message['y'])))
 else:
     print('Cannot find /turtle1/pose. Exit')
